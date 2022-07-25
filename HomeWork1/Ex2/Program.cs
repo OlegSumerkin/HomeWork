7b51e83e8ee66e0c6ddb003d58944a5dcbc08937
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-07-25 19:05:36 +0300

    ex4 complete

--- HomeWork1/Ex2/Program.cs
+++ HomeWork1/Ex2/Program.cs
@@ -9,4 +9,3 @@ int max = number1;
 if(number2 > max) max = number2;
 if(number3 > max) max = number3;
 Console.WriteLine(max);
-
---
+++ HomeWork1/Ex4/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(" | ");
+for(int i=1; i<number; i++){
+    if(i%2==0){
+        Console.Write(i);
+        Console.Write(" | ");
+    }
+}
