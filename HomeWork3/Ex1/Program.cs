46696fccb360a26d6d2e8136b74b82d08349739f
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-07-26 01:20:39 +0300

    ex1.HM3 done but doesn't work

---
+++ HomeWork3/Ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] a = new int[5];
+for(int i=0; i<5; i++){
+    Console.WriteLine("Enter 5-symbol number. {0} symbol: ", i+1);
+    a[i] = Convert.ToInt32(Console.ReadLine());}
+if(a[0]!=a[4]&a[1]!=a[3]) Console.WriteLine("It's not palindrome :( Try again. ");
+        do{
+            Console.WriteLine("It's not palindrome :( Try again. ");
+                for(int i=0; i<5; i++){ 
+                    Console.WriteLine("Enter 5-symbol number. {0} symbol: ", i+1);
+                    a[i] = Convert.ToInt32(Console.ReadLine());}}
+        while(a[0]!=a[4]&a[1]!=a[3]);
+if(a[0]==a[4]&a[1]==a[3]) Console.WriteLine("You're Great! It's palyndrome.");
