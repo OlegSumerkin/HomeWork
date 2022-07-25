abf74698c7095e1be8960c790c88ca2137906791
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-07-26 01:40:17 +0300

    ex3.HM3 complete

---
+++ HomeWork3/Ex3/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(" | ");
+for(int i=1; i<=number; i++){
+    Console.Write(Math.Pow(i, 3));
+    Console.Write(" | ");}
