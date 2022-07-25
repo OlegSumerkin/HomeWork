9be7459b6615879a03a1f56155da3a4f09b20d3e
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-07-25 22:17:14 +0300

    ex2.HM2 complete

---
+++ HomeWork2/Ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter element numbers: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] a = new int[n];
+int central = 0;
+for(int i=0; i<n; i++){
+    Console.WriteLine("Enter value of {0} elemnt: ", i+1);
+    a[i] = Convert.ToInt32(Console.ReadLine());
+}
+if(n<3){
+    central = n/2;
+    Console.WriteLine("There are no third element");}
+else{
+    Console.WriteLine(a[2]);
+}
