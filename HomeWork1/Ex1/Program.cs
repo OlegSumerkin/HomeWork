3dc1faa127cdf3641510a65d2cd545758b0789e4
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-07-25 18:20:01 +0300

    ex1 complete

---
+++ HomeWork1/Ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Input number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number2 > number1)
+{
+    Console.WriteLine(number2);
+}
+else
+{
+    Console.WriteLine(number1);
+}
