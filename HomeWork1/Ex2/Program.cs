7e98a2708e432d16dfb6dc53d84efd82032f6acf
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-07-25 18:30:36 +0300

    ex2 complete

---
+++ HomeWork1/Ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Input number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if(number2 > max) max = number2;
+if(number3 > max) max = number3;
+Console.WriteLine(max);
+
