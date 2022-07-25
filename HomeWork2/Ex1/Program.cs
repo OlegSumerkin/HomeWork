7f3637b0463162b4a4c2a7c90d8b10ac95502835
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-07-25 22:12:25 +0300

    Homework 2 ex1 complete

---
+++ HomeWork2/Ex1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter element numbers: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] a = new int[n];
+int central = 0;
+for(int i=0; i<n; i++){
+    Console.WriteLine("Enter value of {0} elemnt: ", i+1);
+    a[i] = Convert.ToInt32(Console.ReadLine());
+}
+central = n/2;
+Console.WriteLine(a[central]);
