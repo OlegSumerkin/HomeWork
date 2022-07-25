a10a6734ce1c56364d10d2fbe3af462246cbf3ef
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-07-25 18:39:12 +0300

    ex3 complete

---
+++ HomeWork1/Ex3/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number%2==0){
+    Console.Write("Yes");}
+else{
+    Console.Write("No");}
