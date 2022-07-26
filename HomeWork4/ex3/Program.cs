cac87a8eece0503da41b4bf3bdd9434b2661168e
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-07-26 22:43:42 +0300

    ex3.HM4 complete

---
+++ HomeWork4/ex3/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = new int[8];
+Random rand = new Random();
+Console.Write("[");
+for (int i = 0; i < 8; i++){
+    array[i] = rand.Next(9);
+    Console.Write(array[i]);
+    if(i<7)Console.Write(", ");}
+Console.Write("]");
