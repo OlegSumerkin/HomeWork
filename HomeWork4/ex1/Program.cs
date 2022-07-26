1ccbbeb1cb7742e18ef79a9e906c023544bed590
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-07-26 21:36:54 +0300

    ex1.HM4 and ex2.HM4 complete

---
+++ HomeWork4/ex1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Input number: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number's degree: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write("Answer is ");
+Console.Write(Math.Pow(A, B));
