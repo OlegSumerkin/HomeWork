ff19d67aa35928fd769268461bb0c0ddc505ead6
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-07-28 16:42:18 +0300

    ex2.HM5 complete

---
+++ HomeWork5/ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = new int[8];
+Random rand = new Random();
+int summ=0;
+Console.Write("We got array: ");
+Console.Write("[");
+    for (int i = 0; i < 8; i++){
+        array[i] = rand.Next(100, 999);
+        Console.Write(array[i]);
+        if(i<7)Console.Write(", ");}
+        Console.Write("] ");
+            for(int i=1;i<array.Length;i=i+2){
+                summ=summ+array[i];}
+Console.WriteLine($"\nThe summ of values of odd indexes is: {summ}");
