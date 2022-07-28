9723047ef5c92417541a7ed71117ae682585ffc9
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-07-28 15:54:39 +0300

    ex1 complete

---
+++ HomeWork5/Ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = new int[8];
+Random rand = new Random();
+int count=0;{
+Console.Write("[");
+for (int i = 0; i < 8; i++){
+    array[i] = rand.Next(100, 999);
+    Console.Write(array[i]);
+    if(i<7)Console.Write(", ");}
+Console.Write("]");
+    for(int i=0;i<array.Length;i++){
+        if(array[i]%2==0) count=count+1;}
+Console.Write($" =>  {count}");}
