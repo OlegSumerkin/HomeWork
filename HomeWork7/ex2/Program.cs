98f1d316e0c12dd8870ebe98c89c55b6057618c9
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-07-31 13:09:12 +0300

    ex2.HM7 complete

--- HomeWork7/ex2/Program.cs
+++ HomeWork7/ex2/Program.cs
@@ -11,22 +11,18 @@ n=Convert.ToInt32(Console.ReadLine());
 double[,] matrix=new double[m,n];
 for(int i=0;i<m;i++){
     for(int j=0;j<n;j++){
-        matrix[i,j]=rnd.Next(-100, 100)/10.0;
+        matrix[i,j]=rnd.Next(0, 10);
         Console.Write($"{matrix[i,j]} ");
     }
 Console.WriteLine();}
 
 int a;
 int b;
-Console.WriteLine("Enter number of string of element: ");
-a=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter number of column of element: ");
-b=Convert.ToInt32(Console.ReadLine());
-if(a<0||a>m||b<0||b>n) do{
-    Console.WriteLine("Entered address doesn't exist. Try again.");
-    Console.WriteLine("Enter number of string of element: ");
-    a=Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Enter number of column of element: ");
-    b=Convert.ToInt32(Console.ReadLine());}
-while (a<m&&b<n||a<0&&b<0);
-Console.WriteLine($"Your value is {matrix[(a-1),(b-1)]}");
+    do{
+        Console.WriteLine("Enter number of string of element: ");
+        a=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter number of column of element: ");
+        b=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Entered address doesn't exist. Try again.");}
+    while (a>m||b>n||a<0||b<0);
+if(a<m&&b<n&&a>0&&b>0)Console.WriteLine($"Your value is {matrix[(a-1),(b-1)]}");
