7ed67a9d79922f0144ef4e70ce2d8ad17edcf27c
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-07-31 13:27:13 +0300

    ex3.HM7 complete

---
+++ HomeWork7/ex3/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int m;
+int n;
+Random rnd=new Random();
+Console.WriteLine("Enter number of strings: ");
+m=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number of columns: ");
+n=Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix=new double[m,n];
+for(int i=0;i<m;i++){
+    for(int j=0;j<n;j++){
+        matrix[i,j]=rnd.Next(0, 10);
+        Console.Write($"{matrix[i,j]} ");
+    }
+Console.WriteLine();}
+
+for(int j=0;j<m;j++){
+    double avg = 0.0;
+    for(int i=0;i<n;i++){
+        avg = avg + matrix[i,j];}
+    Console.WriteLine($"Average of {j+1} column is: {avg/m}");}
+       
