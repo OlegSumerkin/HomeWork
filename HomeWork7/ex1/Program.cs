5978fc806b3725e3713b6db64acb47097f4da5c8
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-07-31 12:15:24 +0300

    ex1.HM7 complete

---
+++ HomeWork7/ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+int m;
+int n;
+Random rnd=new Random();
+Console.WriteLine("Enter number of strings: ");
+m=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number of columns: ");
+n=Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix=new double[m,n];
+for(int i=0;i<m;i++){
+    for(int j=0;j<n;j++){
+        matrix[i,j]=rnd.Next(-100, 100)/10.0;
+        Console.Write($"{matrix[i,j]} ");
+    }
+Console.WriteLine();}
+
+    
