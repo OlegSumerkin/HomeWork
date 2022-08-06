7840f66a6dbddc15c57ffa192b9204f2498c8807
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-08-06 11:10:23 +0300

    ex2.HM8 complete

---
+++ HomeWork8/ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+int m;
+int n;
+Random rnd=new Random();
+Console.WriteLine("Enter number of strings: ");
+m=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number of columns: ");
+n=Convert.ToInt32(Console.ReadLine());
+
+int[] array=new int[m];
+int[,] matrix=new int[m,n];
+for(int i=0;i<m;i++){
+    int sum=0;
+    for(int j=0;j<n;j++){
+        matrix[i,j]=rnd.Next(0, 10);
+        Console.Write($"{matrix[i,j]} ");
+        sum=sum+matrix[i,j];
+        if(j==n-1) array[i]=sum;
+    }
+Console.WriteLine();}
+
+int min=array[0];
+for(int i=1;i<m;i++){
+    if(min>array[i]) min = array[i];}
+Console.WriteLine($"The minimal summ of all elements of each string is {min} and thats summ of all elements of {Array.IndexOf(array, min)+1} string.");
