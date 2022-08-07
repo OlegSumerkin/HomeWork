2f8992be799a644322018ee5ad1c35e0a93bf385
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-08-07 21:05:40 +0300

    ex2.HM9 complete

---
+++ HomeWork9/ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter number1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number2: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if(n>m){
+    int temp=n;
+    n=m;
+    m=temp;
+}
+Console.WriteLine("Result array: ");
+int a=m-n+1;
+int[] array=new int[a];
+for(int i=0;i<array.Length;i++){
+    array[i]=n;
+    n++;}
+int sum=0;
+for(int i=0;i<array.Length;i++){
+    sum+=array[i];
+}
+Console.WriteLine($"Summ of all numbers between {m-a+1} and {m} is: {sum}");
