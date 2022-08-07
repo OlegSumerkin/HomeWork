d219996c80c174a53621c072c4419386c7a8766a
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-08-07 18:47:28 +0300

    ex3.HM9 complete

---
+++ HomeWork9/ex3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Enter number n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+            
+int A(int n, int m){
+if (n == 0) return m + 1;
+if (n != 0 && m == 0) return A(n - 1, 1);
+if (n > 0 && m > 0) return A(n - 1, A(n, m - 1));
+return A(n,m);}
+Console.WriteLine(A(n,m));
